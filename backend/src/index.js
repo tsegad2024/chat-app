f03aec0f050fa09eb89d196211e83718043a1ba2
Tsegaye Duga <tsegad2024@gmail.com>
2025-11-20 08:54:49 +0300

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,43 +1,42 @@
 import express from "express";
-import dotenv from 'dotenv';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
 import path from "path";
+
 import { connectDB } from "./lib/db.js";
-import authRoute from "./routes/auth.router.js"
-import messageRoute from "./routes/message.route.js"
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
-
 dotenv.config();
+
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json());
 app.use(cookieParser());
-
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:5173",
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 
-
-app.use("/api/auth", authRoute);
-app.use("/api/messages", messageRoute);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    const frontendDistPath = path.join(__dirname, "../frontend/dist");
-    
-    // Serve static files
-    app.use(express.static(frontendDistPath));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    // Catch-all route to serve index.html for SPA
-    app.get('/*', (req, res) => {
-        res.sendFile(path.join(frontendDistPath, 'index.html'));
-    });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-app.listen(PORT, () => {
-    console.log("SERVER IS RUNNING ON PORT " + PORT);
-    connectDB();
-})
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
